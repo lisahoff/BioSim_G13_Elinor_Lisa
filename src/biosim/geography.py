08f65328fa8f566838087e2c4362024e1a955ac9
Lisa Hoff <lisast@nmbu.no>
2020-01-08 11:10:55 +0100

    Started on the geography of the island

---
+++ src/biosim/geography.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Elinor skaras og Lisa Hoff"
+__email__ = "elinor9511@gmail.com, lisast@nmbu.no"
+
+class Geography(object):
+    '''
+    The geography of the island.
+    '''
+    def __init__(self):
