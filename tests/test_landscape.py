5c96fd546e5251918a207befe540e78bd730fa8a
Elinor Brede Skårås <elinor.brede.skaras@nmbu.no>
2020-01-15 18:21:35 +0100

    Landscape and Simulation

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -41,14 +41,38 @@ class TestLandscape(unittest.TestCase):
                         OJMO
                         OMMO"""
 
+        lan1 = Lan(geostring1)
+        lan2 = Lan(geostring2)
+        lan3 = Lan(geostring2)
+        lan4 = Lan(geostring2)
+        lan5 = Lan(geostring2)
 
         with pytest.raises(ValueError):
-            landscape, geostring_list = Lan.create_landscape(self, geostring1)
-            landscape, geostring_list = Lan.create_landscape(self, geostring2)
-            landscape, geostring_list = Lan.create_landscape(self, geostring3)
-            landscape, geostring_list = Lan.create_landscape(self, geostring4)
-            landscape, geostring_list = Lan.create_landscape(self, geostring5)
+            landscape, geostring_list = lan1.create_landscape()
+            landscape, geostring_list = lan2.create_landscape()
+            landscape, geostring_list = lan3.create_landscape()
+            landscape, geostring_list = lan4.create_landscape()
+            landscape, geostring_list = lan5.create_landscape()
 
+    def test_string_landscape_types(self):
+        geostring = """\
+                        OOOO
+                        OOOO
+                        OKKO
+                        OOOO"""
+
+        with pytest.raises(ValueError):
+            landscape, geostring_list = Lan.create_landscape(self, geostring)
+
+    def test_string_is_foursquare(self):
+        geostring = """\
+                        OOO
+                        OOOO
+                        OKKO
+                        OOOO"""
+
+        with pytest.raises(ValueError):
+            landscape, geostring_list = Lan.create_landscape(self, geostring)
 
 
 
